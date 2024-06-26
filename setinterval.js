d10d761c9cda9f8e2d14d976202f3758ee4e0997
min-jungeun <mje971115@gmail.com>
2024-06-26 10:50:25 +0900

    순번대로 된 이미지 1초마다 돌려지는 식

--- setinterval.js
+++ setinterval.js
@@ -1,8 +1,21 @@
-let count = 0;
-const navidata = ["네이버", "다음", "구글", "아마존", "깃"];
+// let count = 0;
+// const navidata = ["네이버", "다음", "구글", "아마존", "깃"];
 
+// setInterval(function(){
+//     console.log(navidata[count]);
+//     count++;
+//     count %= navidata.length; // count = count % 10;  -> 리턴
+// }, 500)
+
+
+//자바스크립트경로는 css와 다름. 자바스크립트가 연결된 html 기준으로 처리해야한다.
+let count = 0;
 setInterval(function(){
-    console.log(navidata[count]);
     count++;
-    count %= navidata.length; // count = count % 10;  -> 리턴
-}, 500)
+    count %= 5;
+    document.querySelector("#bannerimg").setAttribute("src",`/banner/${count}.png`)
+}, 1000);
+
+
+
+// $("#bannerimg").attr("src","/banner/1.png") -> 제이쿼리로 표현
